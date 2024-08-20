f9d52c4d1e383643ca624322e67f41faf899d5c2
kowshiq asfat <kowsiq.asfat@gmail.com>
2024-08-21 01:06:32 +0600

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,40 @@ import "./App.css";
 import Footer from "./components/footer";
 import NavBar from "./components/navBar";
 import HomePage from "./pages/home";
+import { useEffect, useState } from "react";
+import { BallTriangle } from "react-loader-spinner";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
   return (
     <>
-      <NavBar />
-      <HomePage />
-      <Footer />
+      {loading ? (
+        <div className=" d-flex justify-content-center align-items-center min-vh-100">
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+          />
+        </div>
+      ) : (
+        <>
+          <NavBar />
+          <HomePage />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
